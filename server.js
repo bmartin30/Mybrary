911d97ca2ce2cbca09ebf9a33ed5dc5d0a4f7c99
bmartin30 <benedick.martin@gmail.com>
2022-06-18 18:02:33 +0800

    Initial Author Route

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const { Router } = require('express')
 const express = require('express')
 const app = express()
 const expressLayout = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
+//routers
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //where are the views coming from. This is a folder
 app.set('layout', 'layouts/layout') //hookup express layouts, what are layouts going to be
 app.use(expressLayout)
 app.use(express.static('public')) //where are our files public to be. This is a folder
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 //Connect to database
 const mongoose = require('mongoose')
@@ -24,6 +28,7 @@ const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/', indexRouter)
+app.use('/', indexRouter) //root route
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
